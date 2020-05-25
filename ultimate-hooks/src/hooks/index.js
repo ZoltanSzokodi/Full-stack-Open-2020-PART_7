370e44a82694b9fa967d97bce2b41e3402c794ba
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2020-05-25 13:58:47 +0200

    7.8: ultimate hooks

---
+++ ultimate-hooks/src/hooks/index.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export const useField = type => {
+  const [value, setValue] = useState('');
+
+  const onChange = event => {
+    setValue(event.target.value);
+  };
+
+  return {
+    type,
+    value,
+    onChange,
+  };
+};
+
+export const useResource = baseUrl => {
+  const [resources, setResources] = useState([]);
+
+  useEffect(() => {
+    axios.get(baseUrl).then(response => {
+      console.log('->', response);
+      setResources(response.data);
+    });
+  }, [baseUrl]);
+
+  const create = resource => {
+    axios.post(baseUrl, resource).then(response => {
+      setResources(resources.concat(response.data));
+    });
+  };
+
+  const service = {
+    create,
+  };
+
+  return [resources, service];
+};
